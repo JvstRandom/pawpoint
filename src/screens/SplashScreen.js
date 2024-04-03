8f21afcbc2f755358660fcfe55fe51e0e536a9aa
salsaarifa <salsabilaarifa54@gmail.com>
2024-04-03 07:23:12 +0700

    konsul page dan splashscreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,27 +1,47 @@
-import { StackActions } from "@react-navigation/native";
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions } from "react-native";
+import { StackActions } from "@react-navigation/native";
 
 class SplashScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {};
     }
 
     componentDidMount() {
-        setTimeout( () => {
+        setTimeout(() => {
             this.props.navigation.dispatch(StackActions.replace('Home'))
         }, 2000);
     }
 
     render() {
         return (
-            <View>
-                <Text>SPLASHSCREEN</Text>
-                
+            <View style={styles.container}>
+                <Image
+                    source={require('./logo.jpg')} 
+                    style={styles.image}
+                    resizeMode="contain"
+                />
             </View>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+    },
+    image: {
+        width: Dimensions.get('window').width * 0.8, 
+        height: Dimensions.get('window').width * 0.8,
+        marginBottom: 20,
+    },
+    text: {
+        marginTop: 25,
+    },
+});
 
 export default SplashScreen;
